3eef90e83fe77e5284d872253c9d3ccbd3ff0cbb
Marcos-Ramoss <mar.stark.99@gmail.com>
2025-03-23 17:56:59 -0400

    teste ultilizando CSV atravez da anotation @CsvSource

--- src/test/java/br/com/marcos/math/ParametrizacaoCsvTeste.java
+++ src/test/java/br/com/marcos/math/ParametrizacaoCsvTeste.java
@@ -3,14 +3,14 @@ package br.com.marcos.math;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName(" Test Math Operations in SimpleMath Class")
-public class SimpleMathTest {
+public class ParametrizacaoCsvTeste {
 
     SimpleMath simpleMath;
 
@@ -38,17 +38,15 @@ public class SimpleMathTest {
 
     @DisplayName("Operation to division")
     @ParameterizedTest
-    @MethodSource("testDivisionInputParameters")
+    @CsvSource({
+            "6D, 2D, 3D",
+            "10D, 2D, 5D",
+            "20D, 2D, 10D"
+    })
     void testDivision(Double firstNumber, Double secondNumber, double expected) {
         Double actual = simpleMath.division(firstNumber, secondNumber);
         assertEquals(expected, actual, () -> firstNumber + " / " + secondNumber + " did not produce: " + expected + " ! ");
     }
 
-    public  static Stream<Arguments> testDivisionInputParameters(){
-        return Stream.of(
-                Arguments.of(6D, 2D, 3D),
-                Arguments.of(10D, 2D, 5D),
-                Arguments.of(20D, 2D, 10D)
-        );
-    }
+
 }
