e5dae11b05674196816f6cbe1bddfe05935d8909
Marcos-Ramoss <mar.stark.99@gmail.com>
2025-03-24 16:21:01 -0400

    refatorando o testes person seguindo REFACTOR do TDD, mantendo um codigo mas legivel sem redundância

--- src/test/java/br/com/marcos/personService/PersonServiceTest.java
+++ src/test/java/br/com/marcos/personService/PersonServiceTest.java
@@ -1,44 +1,43 @@
 package br.com.marcos.personService;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonServiceTest {
 
-    @DisplayName(" When Create a Person with Sucess Shold Return a Person Object")
-    @Test
-    void testCreatePerson_WhenSucess_ShouldReturnPersonObject(){
+    Person person;
 
-        IPersonService service = new PersonService();
-
-        Person person = new Person(
+    @BeforeEach
+    void setup() {
+        person = new Person(
                 "Marcos",
                 "Oliveira",
                 "mar.stark.99@gmail.com",
                 "Manaus - BR",
                 "Masculino"
         );
+    }
+
+    @DisplayName(" When Create a Person with Sucess Shold Return a Person Object")
+    @Test
+    void testCreatePerson_WhenSucess_ShouldReturnPersonObject() {
+
+        IPersonService service = new PersonService();
 
         Person actual = service.createPerson(person);
 
         assertNotNull(actual, () -> "The createPerson() should not have returned null!! ");
 
-}
+    }
 
     @DisplayName(" When Create a Person with Sucess Shold Contains FirstName in Returned Person Object")
     @Test
-    void testCreatePerson_WhenSucess_ShouldConatainsFirstNameReturnedPersonObject(){
+    void testCreatePerson_WhenSucess_ShouldConatainsFirstNameReturnedPersonObject() {
 
         IPersonService service = new PersonService();
-
-        Person person = new Person(
-                "Marcos",
-                "Oliveira",
-                "mar.stark.99@gmail.com",
-                "Manaus - BR",
-                "Masculino"
-        );
 
         Person actual = service.createPerson(person);
 
